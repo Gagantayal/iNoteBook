ac4598f628a10ab4f72ca49ed8f43da4141bb38c
Gagan Tayal <gagantayal2003@gmail.com>
2023-07-20 12:23:51 +0530

    bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,41 @@
 const express = require("express")
 const User = require("../models/User")
 const {body,validationResult} = require("express-validator")
+const bcrypt = require("bcryptjs")
 const router = express.Router()
 
 router.post('/',[
     body('name',"Enter a valid name").isLength({ min: 3 }),
     body('email',"Enter a valid mail").isEmail(),
     body('password',"Enter a valid password").isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    else{
-        const user = User(req.body);
-        user.save().then(
-            res.send(user)
-        ).catch(err=>console.log(err))       
-    }
+        // const user = User(req.body);
+        // user.save().then(
+        //     res.send(user)
+        // ).catch(err=>res.json({
+        //     error:"please enter correct value"
+        // }))
+    try {  
+    let user = await User.findOne({email:req.body.email})
+    if(user){return res.status(400).json({error:"Sorry user already exist"})}
+    const salt = await bcrypt.genSalt(10)
+    const secpass = await bcrypt.hash(req.body.password,salt)
+    User.create({
+        name:req.body.name,
+        password:secpass,
+        email:req.body.email}).then(user => res.send(user)).catch(err=>res.json({
+            error:"please enter correct value",
+            message:err.message
+        })) 
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json("some error has occured")}
 })
+
 
 
 module.exports = router
